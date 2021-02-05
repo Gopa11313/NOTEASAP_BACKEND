28821289645623c47e749ed427c8bfb65e7f9d20
Gopa11313 <gopalaa9819525993@gmail.com>
2021-02-05 17:17:41 +0545

    Done importing bookmark model in app.js

--- app.js
+++ app.js
@@ -2,11 +2,13 @@ const express=require('express');
 const bodyparser=require('body-parser');
 const R_Router=require("./Routes/Register_routes")
 const UploadNote=require("./Routes/UploadNote_routes")
+const Bookmark=require("./Models/BookMark")
 const mongooes=require("./Databse/db")
 const app=express();
 app.use(express.json())
 app.use(R_Router)
 app.use(UploadNote)
+app.use(Bookmark)
 app.use(bodyparser.urlencoded({extended:false}))
 
 app.listen(3000)
