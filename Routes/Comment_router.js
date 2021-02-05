4dfc00a1a6484e28da3988bbd6ec8ee0302fbbb2
Gopa11313 <gopalaa9819525993@gmail.com>
2021-02-05 19:49:00 +0545

    done with Post method in comment router

---
+++ Routes/Comment_router.js
@@ -0,0 +1,33 @@
+const express = require('express');
+const router = express.Router();
+const Comment = require('../Models/Comment');
+const auth = require("../middleware/auth")
+const { check, validationResult } = require('express-validator');
+const { json } = require('body-parser');
+
+router.post('/comment/on/note',
+    [
+        check('userId', "userId must be filled").not().isEmpty(),
+        check('noteId', "NoteId must be filled").not().isEmpty()
+    ],
+    auth.varifyUser,
+    auth.varifyParticularUser, (req, res) => {
+        const errros = validationResult(req);
+        if (errros.isEmpty()) {
+                var post_data=req.body
+                var userId=post_data.userId
+                var noteId=post_data.noteId
+                var comment=post_data.comment
+                var cmnt=Comment({userId:userId,noteId:noteId,comment:comment})
+                cmnt.save().then(function () {
+                    res.status(201).json({ success: true, msg: "Commented" })
+                }).catch(function (e) {
+                    res.status(500).json({ success: false, msg: "error" })
+                })
+        }
+        else{
+            res.status(400).json({succes:true,msg:errros.array()})
+        }
+})
+
+module.exports=router
--- app.js
+++ app.js
@@ -3,12 +3,14 @@ const bodyparser=require('body-parser');
 const R_Router=require("./Routes/Register_routes")
 const UploadNote=require("./Routes/UploadNote_routes")
 const Bookmark=require("./Routes/Bookmark_router")
+const Comment=require("./Routes/Comment_router")
 const mongooes=require("./Databse/db")
 const app=express();
 app.use(express.json())
 app.use(R_Router)
 app.use(UploadNote)
 app.use(Bookmark)
+app.use(Comment)
 app.use(bodyparser.urlencoded({extended:false}))
 
 app.listen(3000)
