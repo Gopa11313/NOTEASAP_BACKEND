36f6138b63d8c1a3770d2f7aed36963cc739b499
Gopa11313 <gopalaa9819525993@gmail.com>
2021-02-05 19:07:56 +0545

    BookMark Delete route

--- Routes/Bookmark_router.js
+++ Routes/Bookmark_router.js
@@ -11,22 +11,34 @@ router.post('/note/bookmark/',
         check('noteId', "NoteId must be filled").not().isEmpty()
     ],
     auth.varifyUser,
-    auth.varifyParticularUser,(req, res) => {
+    auth.varifyParticularUser, (req, res) => {
         const errros = validationResult(req);
         if (errros.isEmpty()) {
-        var post_data = req.body
-        var userId=post_data.userId
-        var noteId=post_data.noteId
-        var data=BookMark({userId:userId,noteId:noteId})
-        data.save().then(function(){
-            res.status(201).json({ success:true ,msg: "Bookmarked" })
-        }).catch(function(e){
-            res.status(500).json({ success:false,msg: "error"})
-        })
+            var post_data = req.body
+            var userId = post_data.userId/////check it and ask
+            var noteId = post_data.noteId
+            var data = BookMark({ userId: userId, noteId: noteId })
+            data.save().then(function () {
+                res.status(201).json({ success: true, msg: "Bookmarked" })
+            }).catch(function (e) {
+                res.status(500).json({ success: false, msg: "error" })
+            })
         }
-        else{
+        else {
             res.status(400).json(errros.array())
             res.send(errros.array())
         }
     })
-    module.exports = router
+
+router.delete('/delete/bookmark/:bookmarkId',
+    auth.varifyUser,
+    auth.varifyParticularUser, 
+    (req, res) => {
+        const bookmarkId = req.params.bookmarkId;
+        BookMark.deleteOne({ _id: bookmarkId }).then(function () {
+            res.status(200).json({ success:true,msg: "Bookmark Successfully deleted" })
+        }).catch(function (e) {
+            res.status(500).json({ success:false,msg: e })
+        })
+    })
+module.exports = router
