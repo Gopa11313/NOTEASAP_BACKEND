55db37f960d59e85806145e0e9f18d3adcb98c57
gopa11313 <gopalaa9819525993@gmail.com>
2021-03-24 10:43:39 +0545

    simple change in uploadnotes_route fro web

--- Routes/UploadNote_routes.js
+++ Routes/UploadNote_routes.js
@@ -54,12 +54,11 @@ router.put("/upload/user/file/:id", auth.varifyUser, (req, res) => {
             res.status(201).json({ success: false, msg: "error" })
         }
         else if (err) {
-            // An unknown error occurred when uploading.
-            //console.log("hello")
+
             res.status(201).json({ success: false, msg: "not gonna happen" })
         }
         else {
-            console.log(req.file +"hello")
+            console.log(req.file + "hello")
             const id = req.params.id
             //console.log("hello")
             file = req.file.filename
@@ -83,6 +82,26 @@ router.get('/get/notes',
             res.status(201).json({ success: false, msg: "Some Error Occurs" })
         })
     })
+router.put('/Update/note',
+    auth.varifyUser,
+    auth.varifyAdminorUser, (req, res) => {
+        var post_data = req.body;
+        const id = req.post_data.id
+        var file = "saas";
+        var level = post_data.level;
+        var subject = post_data.subject;
+        var c_name = post_data.c_name;
+        var topic = post_data.topic;
+        var description = post_data.description;
+        var userId = post_data.userId
+        UploadNotes.updateOne({_id:id},{ file: file, level: level, subject: subject, c_name: c_name, topic: topic, description: description, userId: userId }).then(function (data) {
+            //console.log(data)
+            res.status(200).json({ success: true, data: data })
+        }).catch(function (e) {
+            console.log("here")
+            res.status(201).json({ success: false, msg: "Some Error Occurs" })
+        })
+    })
 router.get('/get/Ownnotes/:userId',
     auth.varifyUser,
     auth.varifyAdminorUser, (req, res) => {
@@ -96,12 +115,11 @@ router.get('/get/Ownnotes/:userId',
         })
     })
 
-router.get('/note/by/notid/:id',
-    auth.varifyUser,
-    auth.varifyAdminorUser, (req, res) => {
+router.get('/note/by/notid/:id',//check auth here
+     (req, res) => {
         const id = req.params.id
-        UploadNotes.find({ _id: id }).then(function (data) {
-            // console.log(data)
+        UploadNotes.findOne({ _id: id }).then(function (data) {
+            console.log(data)
             res.status(200).json({ success: true, data: data })
         }).catch(function (e) {
             console.log("here")
@@ -144,10 +162,63 @@ router.put('/rate/the/note/:id/:ratting/:noofRating',
         const noofRating = req.params.noofRating
         console.log(noofRating)
         UploadNote.updateOne({ _id: id }, { ratting: ratting, noofRating: noofRating }).then(function () {
-            res.status(200).json({ success: true,msg:"Thnak You For Your Ratting" })
+            res.status(200).json({ success: true, msg: "Thnak You For Your Ratting" })
         }).catch(function (e) {
-            res.status(201).json({ success: false,msg:"Somthing went wrong"  })
+            res.status(201).json({ success: false, msg: "Somthing went wrong" })
         })
     }
 )
+
+
+
+router.post('/upload/note/with/file',
+    [
+        //check('file', "please select the file").not().isEmpty(),
+        check('level', "please enter level").not().isEmpty(),
+        check('subject', "please enter subject").not().isEmpty(),
+        check('topic', "please enter topic").not().isEmpty(),
+        check('userId', "UserID is needed").not().isEmpty()
+    ], auth.varifyUser, auth.varifyAdminorUser, (req, res) => {
+        const errors = validationResult(req);
+        console.log(req.body);
+        if (errors.isEmpty()) {
+            uploadfile(req, res, function (err) {
+
+                if (err instanceof multer.MulterError) {
+                    // A Multer error occurred when uploading.
+                    res.status(201).json({ success: false, msg: "error" })
+                }
+                else if (err) {
+
+                    res.status(201).json({ success: false, msg: "not gonna happen" })
+                }
+                else {
+                    var post_data = req.body;
+                    var file = req.file.filename;
+                    console.log(file)
+                    var level = post_data.level;
+                    var subject = post_data.subject;
+                    var c_name = post_data.c_name;
+                    var topic = post_data.topic;
+                    var description = post_data.description;
+                    var ratting = 0.00
+                    var userId = post_data.userId
+                    var data = new UploadNote({ file: file, level: level, subject: subject, c_name: c_name, topic: topic, description: description, ratting: ratting, userId: userId })
+                    data.save().then(function (data) {
+                        res.status(200).json({ success: true, msg: "User Register Success", id: data._id })
+                    }).catch(function (e) {
+                        console.log("here")
+                        res.status(201).json({ success: false, msg: "Some Error Occurs" })
+                    })
+                }
+            })
+        }
+        else {
+            console.log(errors)
+            res.status(201).json({ success: false, msg: "error" })
+        }
+    }
+)
+
+
 module.exports = router
--- app.js
+++ app.js
@@ -8,13 +8,14 @@ const Bookmark=require("./Routes/Bookmark_router")
 const Comment=require("./Routes/Comment_router")
 const mongooes=require("./Databse/db")
 const app=express();
+app.use(express.static(path.join(__dirname, "public")));
+app.use(bodyparser.urlencoded({extended:false}))
 app.use(express.json())
 app.use(cors())
 app.use(R_Router)
 app.use(UploadNote)
 app.use(Bookmark)
 app.use(Comment)
-app.use(express.static(path.join(__dirname, "public")));
-app.use(bodyparser.urlencoded({extended:false}))
+
 
 app.listen(90)
